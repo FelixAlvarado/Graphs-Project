ca4176f61349f6e8227e1f3de1dc70b77f3043e3
FelixAlvarado <felixalvarado653@gmail.com>
2018-07-10 11:39:32 -0700

    finished topological sort

--- lib/topological_sort.rb
+++ lib/topological_sort.rb
@@ -4,4 +4,29 @@ require_relative 'graph'
 
 def topological_sort(vertices)
 
+    sort = []
+    queue = Queue.new
+    vertices.each do |vertex|
+        if vertex.in_edges.empty? 
+            queue.enq(vertex)
+        end 
+    end 
+    until queue.empty? 
+        edges_destroy = []
+        vertex = queue.pop
+        sort << vertex
+        vertex.out_edges.each do |edge|
+            if edge.to_vertex.in_edges.length == 1
+                queue.enq(edge.to_vertex)
+            end 
+            edges_destroy.push(edge)
+        end 
+        edges_destroy.each {|edge| edge.destroy!}
+
+    end 
+  
+    return sort if vertices.length == sort.length 
+    []
 end
+
+
